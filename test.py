04eec2b4e6885fbe6902bd9c7f6a872e68ef9b6a
igor20192 <igor.udovenko2012@meta.ua>
2023-03-30 10:17:10 +0300

    add concurrent moвгду text_translate.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import unittest
-from translate import Translator
 import os
-from text_translate import write_file
+from unittest.mock import patch, mock_open
+from concurrent.futures import ThreadPoolExecutor
+from translate import Translator
+from text_translate import translatios, write_file
 
 
 class TestTranslations(unittest.TestCase):
@@ -13,13 +15,48 @@ class TestTranslations(unittest.TestCase):
 
 
 class TestWriteFile(unittest.TestCase):
-    def test_write_file(self):
+    def setUp(self):
+        self.mock_open = mock_open()
+
+    @patch("main.open", new_callable=self.mock_open)
+    def test_write_file(self, mock_file):
         lang = "ru"
         clean = True
-        write_file(lang, clean)
+
+        # Мокаем результат функции translatios()
+        with patch("main.translatios") as mock_translations:
+            mock_translations.return_value = ["Привет", "Мир"]
+            write_file(lang, clean)
+
+        # Проверяем, что файл создан и записан корректно
+        mock_file.assert_called_once_with("translate.txt", "a")
+        handle = mock_file()
+        handle.write.assert_has_calls(
+            [unittest.mock.call("Привет\n"), unittest.mock.call("Мир\n")]
+        )
+
+    def test_write_file_no_clean(self):
+        lang = "ru"
+        clean = False
+
+        # Мокаем результат функции translatios()
+        with patch("main.translatios") as mock_translations:
+            mock_translations.return_value = ["Привет", "Мир"]
+            write_file(lang, clean)
+
+        # Проверяем, что файл создан и записан корректно
         with open("translate.txt", "r") as f:
             contents = f.read()
-            self.assertTrue("Привет" in contents)
+            self.assertIn("Привет", contents)
+            self.assertIn("Мир", contents)
+
+
+class TestConcurrency(unittest.TestCase):
+    def test_concurrency(self):
+        lang = "ru"
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            future = executor.submit(translatios, lang)
+            self.assertIsInstance(future.result(), list)
 
 
 if __name__ == "__main__":
